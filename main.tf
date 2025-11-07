2c318d4ef87827ddbd07a88169f83507b722db79
Prasad Deshmukh <deshmukhprasad559@gmail.com>
2025-11-07 18:02:37 +0530

    modify code

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ provider "aws" {
 resource "aws_vpc" "myvpc" {
   cidr_block = "30.30.0.0/16"
 }
-resource "aws_vpc" "myvpc" {
+resource "aws_vpc" "myvpc-1" {
   cidr_block = "30.40.0.0/16"
 }
 resource "aws_subnet" "public" {
