195bdaafa7be440fb2519347057ad4be3958de88
Prasad Deshmukh <deshmukhprasad559@gmail.com>
2025-11-08 14:14:51 +0530

    modify code

--- jenssh_config.tf
+++ jenssh_config.tf
@@ -13,6 +13,9 @@ provider "aws" {
 resource "aws_vpc" "myvpc" {
   cidr_block = "30.30.0.0/16"
 }
+resource "aws_vpc" "myvpc-1" {
+  cidr_block = "30.40.0.0/16"
+}
 resource "aws_subnet" "public" {
   vpc_id     = aws_vpc.myvpc.id
   cidr_block = "30.30.1.0/24"
